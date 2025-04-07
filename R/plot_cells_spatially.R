152fcf6d0bbd444c82a212eb3a26bb99161b6f5f
mcmero <7799828+mcmero@users.noreply.github.com>
2025-04-08 09:58:24 +1000

    Allow spatial plotting coloured by arbitrary metadata col name

--- R/plot_cells_spatially.R
+++ R/plot_cells_spatially.R
@@ -15,7 +15,7 @@ library(dplyr)
 #' coordinates (default: "Centroid Y")
 #' @param pointsize Size of points in the plot (default: 0.5)
 #' @param colour_by Column name in colData of spe to colour points by (options:
-#' "cell_type" (default), "cluster" or "tumour")
+#' "cell_type" (default), "cluster" or a column name in the metadata)
 #' @return ggplot2 object
 #' @export
 #' @importFrom dplyr %>%
@@ -28,16 +28,21 @@ plot_cells_spatially <- function(spe,
                                  pointsize = 0.5,
                                  colour_by = "cell_type") {
   stopifnot(cell_type_colname %in% colnames(SingleCellExperiment::colData(spe)))
-  stopifnot(parent_type_colname %in% colnames(SingleCellExperiment::colData(spe))) # nolint: line_length_linter.
-  if (colour_by == "cluster") {
+  stopifnot(parent_type_colname %in%
+              colnames(SingleCellExperiment::colData(spe)))
+
+  if (colour_by == "cell_type") {
+    stopifnot(cell_type_colname %in%
+                colnames(SingleCellExperiment::colData(spe)))
+  } else if (colour_by == "cluster") {
     stopifnot("cluster" %in% colnames(SingleCellExperiment::colData(spe)))
-  }
-  if (colour_by == "tumour") {
-    stopifnot("In.Tumour" %in% colnames(SingleCellExperiment::colData(spe)))
+  } else {
+    stopifnot(colour_by %in% colnames(SingleCellExperiment::colData(spe)))
   }
 
   if (is.null(parent_types)) {
-    parent_types <- unique(SingleCellExperiment::colData(spe)[[parent_type_colname]]) # nolint: line_length_linter.
+    parent_types <-
+      unique(SingleCellExperiment::colData(spe)[[parent_type_colname]])
   }
 
   coords <- SpatialExperiment::spatialCoords(spe) %>% as.data.frame()
@@ -51,19 +56,17 @@ plot_cells_spatially <- function(spe,
     spe_clusters <- SingleCellExperiment::colData(spe)[["cluster"]]
     df <- dplyr::mutate(coords, cluster = as.factor(spe_clusters),
                         parent_type = spe_parent_types)
-
     ncols <- df$cluster %>% unique() %>% length()
-  } else if (colour_by == "tumour") {
-    spe_tumour <- SingleCellExperiment::colData(spe)[["In.Tumour"]]
-    df <- dplyr::mutate(coords, tumour = as.factor(spe_tumour),
-                        parent_type = spe_parent_types)
-
-    ncols <- df$tumour %>% unique() %>% length()
-  } else {
+  } else if (colour_by == "cell_type") {
     spe_cell_types <- SingleCellExperiment::colData(spe)[[cell_type_colname]]
     df <- dplyr::mutate(coords, cell_type = spe_cell_types,
                         parent_type = spe_parent_types)
     ncols <- df$cell_type %>% unique() %>% length()
+  } else {
+    spe_meta <- SingleCellExperiment::colData(spe)[[colour_by]]
+    df <- dplyr::mutate(coords, !!colour_by := as.factor(spe_meta), # nolint: object_usage_linter, line_length_linter.
+                        parent_type = spe_parent_types)
+    ncols <- df[[colour_by]] %>% unique() %>% length()
   }
 
   df <- dplyr::filter(df, parent_type %in% parent_types) # nolint: object_usage_linter, line_length_linter.
