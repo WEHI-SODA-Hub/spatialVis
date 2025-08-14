ff64c34c5e8a78b503a9f859e8f67d6048344fb0
mcmero <7799828+mcmero@users.noreply.github.com>
2025-08-14 15:01:55 +1000

    Allow multiple compartments to be plotted for intensity measurements

--- R/plot_intensity_measurements.R
+++ R/plot_intensity_measurements.R
@@ -6,14 +6,17 @@ library(dplyr)
 #' @description Take a GeoJSON file containing cell segmentation output and
 #' plot intensity measurements for the specified compartment.
 #' @param geojson_file Path to the GeoJSON file containing segmentation data
-#' @param compartment Compartment to plot intensity measurements for (default:
-#' "Cell"). Options are "Cell", "Nucleus", "Cytoplasm", and "Membrane".
+#' @param compartment Compartments to plot intensity measurements for (default:
+#' c("Nucleus", "Cell", "Cytoplasm", "Membrane")). Must be at least one of the
+#' following: "Cell", "Nucleus", "Cytoplasm", "Membrane".
 #' @param calculation Calculation to use for intensity measurements (default:
 #' "Median"). Options are "Mean", "Median", "Min", "Max" and "Std.Dev".
 #' @export
 #' @importFrom dplyr %>%
 plot_intensity_measurements <- function(geojson_file,
-                                        compartment = "Cell",
+                                        compartments =
+                                          c("Nucleus", "Cell",
+                                            "Cytoplasm", "Membrane"),
                                         calculation = "Median") {
   seg <- jsonlite::fromJSON(geojson_file)
 
@@ -22,52 +25,49 @@ plot_intensity_measurements <- function(geojson_file,
     "properties" %in% names(seg$features),
     "objectType" %in% names(seg$features$properties),
     "measurements" %in% names(seg$features$properties),
-    compartment %in% c("Cell", "Nucleus", "Cytoplasm", "Membrane"),
+    all(compartments %in% c("Cell", "Nucleus", "Cytoplasm", "Membrane")),
     calculation %in% c("Mean", "Median", "Min", "Max", "Std.Dev")
   )
 
   properties <- seg$features$properties
   measurement_data <- properties$measurements
 
-  measurement <- paste(compartment, calculation, sep = ": ")
-  col_idx <- which(stringr::str_detect(colnames(measurement_data),
-                                       stringr::fixed(measurement)))
+  # Find measurement columns for all compartments and channels
+  measurements <- paste(compartments, calculation, sep = ": ")
+  col_idxs <- lapply(measurements, function(measurement) {
+    col_idx <- which(stringr::str_detect(colnames(measurement_data),
+                                         stringr::fixed(measurement)))
+    if (length(col_idx) == 0) {
+      warning(paste("No measurements found for", measurement))
+    }
+    col_idx
+  }) %>%
+    unlist()
 
-  if (length(col_idx) == 0) {
-    stop(paste("Measurement", measurement, "not found in data"))
+  if (length(col_idxs) == 0) {
+    stop("No measurements found in data")
   }
 
-  channels <- colnames(measurement_data)[col_idx] %>%
-    stringr::str_split(":") %>%
-    lapply(., first) %>% #nolint
-    unlist() %>%
-    stringr::str_trim()
-
-  if (length(channels) == 0) {
-    stop(paste("No channels found for measurement", measurement))
-  }
-
-  df <- measurement_data[, col_idx] %>%
+  # Format dataframe for plotting
+  df <- measurement_data[, col_idxs] %>%
     dplyr::filter(!if_all(everything(), is.na)) %>%
     tidyr::pivot_longer(everything(),
-                        names_to = "channel",
+                        names_to = "measurement",
                         values_to = "intensity") %>%
-    dplyr::mutate(channel = stringr::str_split(channel, ":") %>% #nolint
-                      lapply(., first) %>% #nolint
-                      unlist() %>%
-                      stringr::str_trim())
+    tidyr::separate(measurement, into = c("channel", "compartment", #nolint
+                                          "calculation"),
+                    sep = ": ", extra = "merge", fill = "right")
 
   pal <- colorRampPalette(
     RColorBrewer::brewer.pal(n = 11, name = "Spectral")
-  )(length(channels))
+  )(length(unique(df$channel)))
 
   ggplot2::ggplot(data = df, ggplot2::aes(x = channel, y = intensity, #nolint
                                           fill = channel)) +
     ggplot2::geom_violin(adjust = 2) +
-    ggplot2::geom_jitter(width = 0.1, alpha = 0.2) +
-    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, hjust = 1)) +
-    ggplot2::ggtitle(paste(compartment, calculation,
-                           "intensity measurements")) +
+    ggplot2::geom_jitter(width = 0.1, alpha = 0.6) +
+    ggplot2::facet_wrap(~ compartment, scales = "free_y") +
+    ggplot2::ggtitle(paste(calculation, "intensity measurements")) +
     ggplot2::scale_fill_manual(values = pal) +
     ggplot2::theme(axis.title.y = ggplot2::element_blank(),
                    axis.text.x = ggplot2::element_text(angle = 90, hjust = 1),
