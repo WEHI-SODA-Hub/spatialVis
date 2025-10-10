d1fddd287e6b2c27436abb376fa4e10bf528692e
mcmero <7799828+mcmero@users.noreply.github.com>
2025-10-10 16:34:15 +1100

    Test plot_geometries with image

--- tests/testthat/test_plot_geometries.R
+++ tests/testthat/test_plot_geometries.R
@@ -7,3 +7,21 @@ test_that("plot_geometries() works", {
 
   expect_is(geometry_plot, "ggplot")
 })
+
+test_that("plot_geometries() works with a background image", {
+  geom_data <- get_segmentation_geometry(
+    "../../inst/extdata/segmentation.geojson"
+  )
+
+  # Download tiff temporarily
+  image_file <- withr::local_tempfile(fileext = ".tiff")
+  download.file(
+    "https://github.com/WEHI-SODA-Hub/spatialproteomics/raw/refs/heads/main/tests/data/mesmer/test_data.tiff", # nolint
+    destfile = image_file,
+    mode = "wb"
+  )
+
+  geometry_plot <- plot_geometries(geom_data, image = image_file)
+
+  expect_is(geometry_plot, "ggplot")
+})
